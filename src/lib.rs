693c785b59cbeba9cb062306ba210ac9f9cf0ee1
Romuald Texier-Marcadé <r.texiermarcade@allo-media.fr>
2020-08-14 16:25:45 +0200

    Access test for english.

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ mod tests {
     use super::{replace_numbers, Language};
 
     #[test]
-    fn test_access() {
+    fn test_access_fr() {
         let french = Language::french();
         assert_eq!(
             replace_numbers(
@@ -20,6 +20,18 @@ mod tests {
                 &french
             ),
             "Pour la 5ème fois: 25 plus 48 égalent 73"
+        );
+    }
+
+    #[test]
+    fn test_access_en() {
+        let english = Language::english();
+        assert_eq!(
+            replace_numbers(
+                "For the fifth time: twenty-five plus fourty-eight equal seventy-three",
+                &english
+            ),
+            "For the 5th time: 25 plus 48 equal 73"
         );
     }
 }
